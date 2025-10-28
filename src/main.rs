9240dbac5909a8493b2e1d8f091836485ff3862e
ByteAtATime <ByteAtATime@proton.me>
2025-10-28 11:10:33 -0700

    feat: add rustyscript to run basic js

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use rustyscript::{Module, Runtime, RuntimeOptions};
+
+fn main() -> Result<(), rustyscript::Error> {
+    let mut runtime = Runtime::new(RuntimeOptions {
+        ..Default::default()
+    })?;
+
+    let module = Module::new(
+        "setup.js",
+        "
+        import { createRequire } from 'module';
+        const nodeRequire = createRequire(import.meta.url);
+
+        globalThis.require = (moduleName) => {
+            return nodeRequire(moduleName);
+        };
+        
+        globalThis.module = { exports: {} };
+        ",
+    );
+
+    let module2 = Module::new(
+        "plugin.js",
+        "
+        const fs = require('fs');
+        module.exports = {
+            content: fs.readFileSync('./src/main.rs', 'utf-8')
+        };
+        ",
+    );
+
+    let module3 = Module::new(
+        "module.js",
+        "
+        console.log(module.exports.content);
+        ",
+    );
+
+    runtime.load_module(&module)?;
+    runtime.load_module(&module2)?;
+    runtime.load_module(&module3)?;
+    Ok(())
 }
