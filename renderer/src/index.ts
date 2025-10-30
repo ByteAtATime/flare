fc31d175235cc890ab2c0b481642fa0a34124394
ByteAtATime <ByteAtATime@proton.me>
2025-10-29 19:31:25 -0700

    feat: add launchType environment to renderer

--- renderer/src/index.ts
+++ renderer/src/index.ts
@@ -1,9 +1,20 @@
 import React from "react";
+import type * as RaycastApiType from "@raycast/api";
+
+const LaunchType = {
+  UserInitiated: "userInitiated",
+  Background: "background",
+} as const;
 
 const raycastApi = {
   showToast: (message: string) => {
     console.log(`ooh toast: ${message}`);
   },
+  LaunchType,
+  environment: {
+    launchType:
+      LaunchType.UserInitiated as RaycastApiType.LaunchType.UserInitiated,
+  } satisfies Partial<RaycastApiType.Environment>,
 };
 
 export { React, raycastApi };
