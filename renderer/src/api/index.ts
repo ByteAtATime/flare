22e519f286a7bde564ef2fe7570b6340299579d3
ByteAtATime <byteatatime@proton.me>
2025-11-22 17:55:46 -0800

    refactor: create api index file

---
+++ renderer/src/api/index.ts
@@ -0,0 +1,23 @@
+import { Grid, ActionPanel, Action, Detail } from "./components";
+import { Toast } from "./toast";
+import { Cache } from "./cache";
+import { LaunchType, environment, getPreferenceValues } from "./environment";
+import { useNavigation } from "../navigation";
+import { Icon } from "../icons";
+
+const raycastApi = {
+  showToast: (message: string) => {},
+  Grid,
+  ActionPanel,
+  Action,
+  Detail,
+  LaunchType,
+  useNavigation,
+  environment,
+  Toast,
+  Cache,
+  getPreferenceValues,
+  Icon,
+};
+
+export { raycastApi };
