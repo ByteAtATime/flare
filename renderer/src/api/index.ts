d9f1006bb0bc8dbf255f6221c26520cafcc57fee
ByteAtATime <byteatatime@proton.me>
2025-11-26 15:53:07 -0800

    feat: add color definitions to api

--- renderer/src/api/index.ts
+++ renderer/src/api/index.ts
@@ -19,6 +19,18 @@ const raycastApi = {
   Cache,
   getPreferenceValues,
   Icon,
+  Color: {
+    Blue: "raycast-blue",
+    Green: "raycast-green",
+    Magenta: "raycast-magenta",
+    Orange: "raycast-orange",
+    Purple: "raycast-purple",
+    Red: "raycast-red",
+    Yellow: "raycast-yellow",
+
+    PrimaryText: "raycast-primary-text",
+    SecondaryText: "raycast-secondary-text",
+  },
 };
 
 export { raycastApi };
