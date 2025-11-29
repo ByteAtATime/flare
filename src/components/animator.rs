29cbeb57cadb8ffd4684475bcf5256c6c5c52dd2
ByteAtATime <byteatatime@proton.me>
2025-11-28 20:53:51 -0800

    fix: delegate functions to root widget in scale to keep behavior

--- src/components/animator.rs
+++ src/components/animator.rs
@@ -76,6 +76,64 @@ impl<Message> Widget<Message, Theme, Renderer> for Scaler<'_, Message> {
             );
         });
     }
+
+    fn mouse_interaction(
+        &self,
+        state: &widget::Tree,
+        layout: Layout<'_>,
+        cursor: iced::advanced::mouse::Cursor,
+        viewport: &Rectangle,
+        renderer: &Renderer,
+    ) -> iced::advanced::mouse::Interaction {
+        self.content
+            .as_widget()
+            .mouse_interaction(state, layout, cursor, viewport, renderer)
+    }
+
+    fn overlay<'a>(
+        &'a mut self,
+        state: &'a mut widget::Tree,
+        layout: Layout<'a>,
+        renderer: &Renderer,
+        viewport: &Rectangle,
+        translation: iced::Vector,
+    ) -> Option<iced::advanced::overlay::Element<'a, Message, Theme, Renderer>> {
+        self.content
+            .as_widget_mut()
+            .overlay(state, layout, renderer, viewport, translation)
+    }
+
+    fn operate(
+        &mut self,
+        state: &mut widget::Tree,
+        layout: Layout<'_>,
+        renderer: &Renderer,
+        operation: &mut dyn widget::Operation,
+    ) {
+        self.content
+            .as_widget_mut()
+            .operate(state, layout, renderer, operation);
+    }
+
+    fn size_hint(&self) -> Size<Length> {
+        self.content.as_widget().size_hint()
+    }
+
+    fn update(
+        &mut self,
+        state: &mut widget::Tree,
+        event: &iced::Event,
+        layout: Layout<'_>,
+        cursor: iced::advanced::mouse::Cursor,
+        renderer: &Renderer,
+        clipboard: &mut dyn iced::advanced::Clipboard,
+        shell: &mut iced::advanced::Shell<'_, Message>,
+        viewport: &Rectangle,
+    ) {
+        self.content.as_widget_mut().update(
+            state, event, layout, cursor, renderer, clipboard, shell, viewport,
+        );
+    }
 }
 
 impl<'a, Message> From<Scaler<'a, Message>> for Element<'a, Message, Theme, Renderer>
